7557d5f709b811004bf91e4ceb865077fc943d15
Ytkagus <Ytkagus321@gmail.com>
2023-04-15 19:41:27 +0500

    0.1

---
+++ Quadrocopter/Assets/Scripts/Magnit.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Magnit : MonoBehaviour
+{
+    Transform connected;
+    Vector3 offset;
+    bool isPresed = false;
+
+    private void Start()
+    {
+        isPresed = false;
+    }
+    private void Update()
+    {
+        if ((connected != null) && (isPresed = true))
+        {
+            connected.position = transform.position + offset;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isPresed)
+        {
+            connected = collision.transform;
+            offset = connected.position - transform.position;
+        }
+        
+    }
+    public void OnPress()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            isPresed = true;
+        }
+    }
+    public void OnRelease()
+    {
+        if (Input.GetKeyUp(KeyCode.R))
+        { 
+            isPresed = false;
+            connected = null;
+        }
+        
+    }
+}
